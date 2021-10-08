a6687f874e68fa335e60edb08cf10de1845c7d19
rbender4 <55886886+rbender4@users.noreply.github.com>
2021-10-08 14:49:30 -0400

    Add files via upload

--- website/src/App.js
+++ website/src/App.js
@@ -1,13 +1,34 @@
 import React, { Component } from "react";
-import Movies from "./components/movies.jsx";
+import { Route, Redirect, Switch } from "react-router-dom";
+import Recipes from "./components/recipes";
+import RecipeForm from "./components/recipeForm";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
+import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <main className="container">
-        <Movies />
-      </main>
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/register" component={RegisterForm}></Route>
+            <Route path="/login" component={LoginForm}></Route>
+            <Route path="/recipes/:id" component={RecipeForm}></Route>
+            <Route path="/recipes" component={Recipes}></Route>
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/rentals" component={Rentals}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Redirect from="/" exact to="/recipes" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
