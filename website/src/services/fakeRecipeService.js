160ff415ff813a109d03699b8f39f5e58a8ef8f4
rbender4 <55886886+rbender4@users.noreply.github.com>
2021-10-08 14:48:55 -0400

    Add files via upload

---
+++ website/src/services/fakeRecipeService.js
@@ -0,0 +1,97 @@
+import * as genresAPI from "./fakeGenreService";
+
+const recipes = [
+  {
+    _id: "5b21ca3eeb7f6fbccd471815",
+    title: "Terminator",
+    genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
+    numberInStock: 6,
+    dailyRentalRate: 2.5,
+    publishDate: "2018-01-03T19:04:28.809Z",
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471816",
+    title: "Die Hard",
+    genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
+    numberInStock: 5,
+    dailyRentalRate: 2.5,
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471817",
+    title: "Get Out",
+    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
+    numberInStock: 8,
+    dailyRentalRate: 3.5,
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471819",
+    title: "Trip to Italy",
+    genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
+    numberInStock: 7,
+    dailyRentalRate: 3.5,
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd47181a",
+    title: "Airplane",
+    genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
+    numberInStock: 7,
+    dailyRentalRate: 3.5,
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd47181b",
+    title: "Wedding Crashers",
+    genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
+    numberInStock: 7,
+    dailyRentalRate: 3.5,
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd47181e",
+    title: "Gone Girl",
+    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
+    numberInStock: 7,
+    dailyRentalRate: 4.5,
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd47181f",
+    title: "The Sixth Sense",
+    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
+    numberInStock: 4,
+    dailyRentalRate: 3.5,
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471821",
+    title: "The Avengers",
+    genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
+    numberInStock: 7,
+    dailyRentalRate: 3.5,
+  },
+];
+
+export function getRecipes() {
+  return recipes;
+}
+
+export function getRecipe(id) {
+  return recipes.find((m) => m._id === id);
+}
+
+export function saveRecipe(recipe) {
+  let recipeInDb = recipes.find((m) => m._id === recipe._id) || {};
+  recipeInDb.title = recipe.title;
+  recipeInDb.genre = genresAPI.genres.find((g) => g._id === recipe.genreId);
+  recipeInDb.numberInStock = recipe.numberInStock;
+  recipeInDb.dailyRentalRate = recipe.dailyRentalRate;
+
+  if (!recipeInDb._id) {
+    recipeInDb._id = Date.now().toString();
+    recipes.push(recipeInDb);
+  }
+
+  return recipeInDb;
+}
+
+export function deleteRecipe(id) {
+  let recipeInDb = recipes.find((m) => m._id === id);
+  recipes.splice(recipes.indexOf(recipeInDb), 1);
+  return recipeInDb;
+}
